997d96dec4035b83c665183fa76845d4258ed2ff
Abraham Rodriguez <arm.developer00@gmail.com>
2025-03-11 16:46:17 +0100

    arreglos

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/postcss";
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server: {
-		port: 3001,
-	},
+    plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [tailwindcss(), require("autoprefixer")],
+        },
+    },
+    server: {
+        port: 3001,
+    },
 });
